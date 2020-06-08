9a4328cf19b5785bcb84ff52c93eb6de5ed2b0bd
CHEICK KANTE <kocheick1@gmail.com>
2020-06-08 12:49:38 -0400

    Pawffe Cat Caffe homework -- test knowledge of polymorphism, inheritance and relationships between classes

---
+++ Week2/PawfeeCatCaffe/src/model/people/Employee.kt
@@ -0,0 +1,34 @@
+package model.people
+
+import java.text.SimpleDateFormat
+
+class Employee(
+    firstName: String,
+    lastName: String,
+    email: String,
+    phoneNumber: String,
+    val salary: Double,
+    val socialSecurityNumber: String,
+    val hireDate: String
+) : Person(firstName = firstName, lastName = lastName, email = email, phoneNumber = phoneNumber) {
+
+    override fun toString(): String {
+        return "" // TODO format the data in any way you want! :]
+    }
+
+    /**
+     * Prints a time of clocking in, in a nice format.
+     *
+     * Hint: to get time, you can create a `Date` object. Use SimpleDateFormatter to format the time!
+     * */
+    fun clockIn() {
+        val date = SimpleDateFormat("yyyy.MM.dd G")
+        println("Clocked in at $date")
+    }
+
+    // TODO same as above, but times for clocking out!
+    fun clockOut() {
+        val date = SimpleDateFormat("yyyy.MM.dd G")
+        println("Clock-out at $date")
+    }
+}
