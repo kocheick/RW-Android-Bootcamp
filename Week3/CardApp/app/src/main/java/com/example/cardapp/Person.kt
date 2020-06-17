eca4a50b1b0ff2a1e60c55c18d7d851e2503f3d0
Cheick K <kocheick1@gmail.com>
2020-06-17 15:39:46 -0400

    Adding week 3, first android app from scratch

---
+++ Week3/CardApp/app/src/main/java/com/example/cardapp/Person.kt
@@ -0,0 +1,4 @@
+package com.example.cardapp
+
+class Person(val fName: String, val jobTitle: String, val city : String, val schedule: String, val phone: String, val email : String, val social: String ) {
+}
