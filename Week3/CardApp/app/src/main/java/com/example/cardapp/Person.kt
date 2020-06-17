796ef5eda507e38923ffd272daac4db4c674b0be
Cheick K <kocheick1@gmail.com>
2020-06-17 18:01:35 -0400

    added app icon

--- Week3/CardApp/app/src/main/java/com/example/cardapp/Person.kt
+++
@@ -1,4 +0,0 @@
-package com.example.cardapp
-
-class Person(val fName: String, val jobTitle: String, val city : String, val schedule: String, val phone: String, val email : String, val social: String ) {
-}
