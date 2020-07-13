fa7ae7ca9341140a95ed4fa5052e550e79bbe4bf
Cheick K <kocheick1@gmail.com>
2020-07-13 19:55:48 -0400

    Login screen activity added

--- Week4/movie-app/app/src/main/java/com/example/mymovieinfo/MainActivity.kt
+++ Week4/movie-app/app/src/main/java/com/example/mymovieinfo/MainActivity.kt
@@ -1,24 +1,69 @@
 package com.example.mymovieinfo
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     private val adapter = MovieAdapter(MovieStore.getAllMovies())
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initMovie()
-
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        //supportActionBar?.setDisplayShowHomeEnabled(false)
     }
 
     private fun initMovie() {
-        movieListRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false)
+        movieListRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         movieListRecyclerView.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.user_state, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                logOut()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun logOut() {
+
+        val editor: Editor = getSharedPreferences("state", Context.MODE_PRIVATE).edit()
+        editor.putBoolean("loggedStatus", false).apply()
+
+        val intent = Intent(this, LoginActivity::class.java)
+            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishAffinity()
     }
 }
